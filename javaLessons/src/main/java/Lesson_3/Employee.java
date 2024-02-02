b2eaa067ad3b658b1c4ff22bcc98f2704f1e1ed1
UladzilsauLazakovich <uladzilsau.lazakovich@gmail.com>
2024-02-02 16:04:25 +0300

    Java class

--- javaLessons/src/main/java/Lesson_3/Employee.java
+++ javaLessons/src/main/java/Lesson_3/Employee.java
@@ -1,32 +1,32 @@
 package Lesson_3;
 
 public class Employee {
-    private String fullName;
-    private String position;
-    private String email;
-    private String phone;
-    private int salary;
-    private int age;
+    private final String fullName;
+    private final String position;
+    private final String email;
+    private final String phoneNumber;
+    private final int salary;
+    private final int age;
 
     // Конструктор класса
-    public Employee(String fullName, String position, String email, String phone, int salary, int age) {
+    public Employee(String fullName, String position, String email, String phoneNumber, int salary, int age) {
         this.fullName = fullName;
         this.position = position;
         this.email = email;
-        this.phone = phone;
+        this.phoneNumber = phoneNumber;
         this.salary = salary;
         this.age = age;
     }
 
-    // Метод для вывода информации о сотруднике
+    // Метод вывода информации о сотруднике
     public void displayInfo() {
         System.out.println("Full Name: " + fullName);
         System.out.println("Position: " + position);
         System.out.println("Email: " + email);
-        System.out.println("Phone: " + phone);
+        System.out.println("Phone Number: " + phoneNumber);
         System.out.println("Salary: " + salary);
         System.out.println("Age: " + age);
-        System.out.println("-----------------------------");
+        System.out.println();
     }
 
     // Геттер для возраста
