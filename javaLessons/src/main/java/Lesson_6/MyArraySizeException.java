e0e85a91a63ae7fe050bc88f2a18c7c2d7af3bd4
UladzilsauLazakovich <uladzilsau.lazakovich@gmail.com>
2024-02-16 13:05:02 +0300

    Java class

---
+++ javaLessons/src/main/java/Lesson_6/MyArraySizeException.java
@@ -0,0 +1,51 @@
+package Lesson_6;
+
+class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
+
+class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
+
+class ArrayProcessor {
+    public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        int sum = 0;
+
+        // Проверка размера массива
+        if (array.length != 4 || array[0].length != 4)
+            throw new MyArraySizeException("Массив должен быть размером 4x4");
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "]");
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        String[][] array = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            int sum = processArray(array);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
+        }
+    }
+}
